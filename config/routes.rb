d4c7d5735dbc03574aedbf8c5eabfd0276268fd6
Fragarias <148366640+Fragarias@users.noreply.github.com>
2024-02-12 11:42:12 +0000

    [Add] devise用routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :end_users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_for :end_users, skip: [:passwords], controllers: { #sign_up sign_inのみ
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+
+  devise_for :admins, skip: [:registrations, :passwords], controllers: { #sign_inのみ
+    sessions: "admin/sessions"
+  }
+
 end
