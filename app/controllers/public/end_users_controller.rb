1f4055d8516ddf36e77b3d85a9dfcdd86a959715
Fragarias <148366640+Fragarias@users.noreply.github.com>
2024-02-12 20:07:28 +0000

    [Add] 管理者一般ユーザ/ログイン機能

--- app/controllers/public/end_users_controller.rb
+++ app/controllers/public/end_users_controller.rb
@@ -7,8 +7,26 @@ class Public::EndUsersController < ApplicationController
   end
 
   def edit
+    @end_user = current_end_user
   end
 
   def quit
+    @end_user = current_end_user
   end
+
+  def update
+    @end_user = current_end_user
+    if @end_user.update(end_user_params)
+      #flash[:notice] = "編集内容を保存しました。"
+      redirect_to end_user_path(@end_user.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def end_user_params
+    params.require(:end_user).permit(:name, :introduction, :email)
+  end
+
 end
