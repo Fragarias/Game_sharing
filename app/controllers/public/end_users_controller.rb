7320343b9303f1ff296d8145ecd624f4eb922add
Fragarias <148366640+Fragarias@users.noreply.github.com>
2024-02-17 01:56:34 +0000

    [Update] public-EndUser-view

--- app/controllers/public/end_users_controller.rb
+++ app/controllers/public/end_users_controller.rb
@@ -3,9 +3,11 @@ class Public::EndUsersController < ApplicationController
   before_action :ensure_guest_user, only: [:edit]
 
   def index
+    @end_users = EndUser.all
   end
 
   def show
+    @end_user = EndUser.find(params[:id])
   end
 
   def edit
@@ -19,19 +21,27 @@ class Public::EndUsersController < ApplicationController
   def update
     @end_user = current_end_user
     if @end_user.update(end_user_params)
-      #flash[:notice] = "編集内容を保存しました。"
+      flash[:notice] = "編集内容を保存しました。"
       redirect_to end_user_path(@end_user.id)
     else
       render :edit
     end
   end
 
-  private
-  def end_user_params
-    params.require(:end_user).permit(:name, :introduction, :email)
+  def withdraw
+    end_user = current_end_user
+    end_user.update(is_active: false)
+    reset_session
+    flash[:notice] = "退会しました。"
+    redirect_to root_path
   end
 
-  def ensure_guest_user
+  private
+  def end_user_params
+    params.require(:end_user).permit(:profile_image, :name, :introduction, :email)
+  end
+
+  def ensure_guest_user #guestuserログイン用
     @end_user = Enduser.find(params[:id])
     if @end_user.name == "guestuser"
       redirect_to end_user_path(current_user) , notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
