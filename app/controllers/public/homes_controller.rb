6fb8b3e247c001cc710868ce45ace2198eca277a
Fragarias <148366640+Fragarias@users.noreply.github.com>
2024-04-02 17:02:23 +0000

    [Fix] 検索機能　非公開/削除済みを非表示

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,9 +1,10 @@
 class Public::HomesController < ApplicationController
   def top # order('id DESC')...新着順（id降順）
+    posts = Post.where(is_deleted: false, is_published: true)
     if params[:word]
-      @new_posts = Post.where('title || text LIKE ?', "%#{params[:word]}%", is_deleted: false, is_published: true).order('id DESC').page(params[:page]).per(8)
+      @new_posts = posts.where('title || text LIKE ?', "%#{params[:word]}%").order('id DESC').page(params[:page]).per(8)
     else
-      @new_posts = Post.where(is_deleted: false, is_published: true).order('id DESC').page(params[:page]).per(8)
+      @new_posts = posts.order('id DESC').page(params[:page]).per(8)
     end
   end
 end
