2f1766adb477cf9e2f98474c4b459e3df58bab86
Fragarias <148366640+Fragarias@users.noreply.github.com>
2024-06-02 13:37:35 +0000

    [Update] home画面ソート機能追加

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,10 +1,18 @@
 class Public::HomesController < ApplicationController
-  def top # order('id DESC')...新着順（id降順）
+  def top # order('id DESC')...新着順（id降順）.  .page(params[:page]).per(8)
     posts = Post.where(is_deleted: false, is_published: true)
     if params[:word]
-      @new_posts = posts.where('title || text LIKE ?', "%#{params[:word]}%").order('id DESC').page(params[:page]).per(8)
+      @new_posts = posts.where('title || text LIKE ?', "%#{params[:word]}%").order('id DESC')
+    elsif params[:latest]
+      @new_posts = posts.latest
+    elsif params[:old]
+      @new_posts = posts.old
+    elsif params[:like_count]
+      @new_posts = posts.like_count
+    elsif params[:random]
+      @new_posts = posts.random
     else
-      @new_posts = posts.order('id DESC').page(params[:page]).per(8)
+      @new_posts = posts.order('id DESC')
     end
   end
 end
